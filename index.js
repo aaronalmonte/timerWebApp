0bf9dc0a3135fe5b73ffa6c1fbead8916f87dd1b
Aaron Almonte <aaron.almontes@gmail.com>
2020-01-12 22:15:06 -0500

    html: added start/pause buttons and number input. js: created Timer class, defined constructor, start and pause method

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hi there!");
+class Timer {
+    constructor (durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+       this.IntervalId = null;
+
+        this.startButton.addEventListener('click', this.IntervalId = this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+    start = () => {
+        this.tick();
+        this.intervalId = setInterval(this.tick, 1000);
+    }
+    pause = () => {
+        clearInterval(this.intervalId);
+
+    }
+    // onDurationChange () {
+
+    // }
+    tick = () => {
+        console.log("Tick");
+    }
+}
+
+ const durationInput = document.querySelector('#duration');
+ const startButton = document.querySelector('#start');
+ const pauseButton = document.querySelector('#pause');
+
+ const timer = new Timer(durationInput, startButton, pauseButton);
