7e063ba3accad36ccd7e8dc21e4521dfd099a944
Aaron Almonte <aaron.almontes@gmail.com>
2020-01-13 00:16:57 -0500

    js: Timer class: modified class so if can receive callbacks to communicate events outside of the class

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 class Timer {
-    constructor (durationInput, startButton, pauseButton) {
+    constructor (durationInput, startButton, pauseButton, callbacks) {
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
         this.IntervalId = null;
+        if (callbacks){
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
 
-        this.startButton.addEventListener('click', this.IntervalId = this.start);
+        this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
     }
     start = () => {
+        if(this.onStart) {
+            this.onStart();
+        }
         this.tick();
         this.intervalId = setInterval(this.tick, 1000);
     }
@@ -21,10 +29,16 @@ class Timer {
     // }
     tick = () => {
         if(this.timeRemaining > 0) {
-        this.timeRemaining = this.timeRemaining - 1;
+            this.timeRemaining = this.timeRemaining - 1;
+            if(this.onTick) {
+                this.onTick();
+            }
         }
         else { 
             this.pause();
+            if(this.onComplete) {
+                this.onComplete();
+            }
         }
     }
 
@@ -41,4 +55,15 @@ class Timer {
  const startButton = document.querySelector('#start');
  const pauseButton = document.querySelector('#pause');
 
- const timer = new Timer(durationInput, startButton, pauseButton);
+ const timer = new Timer(durationInput, startButton, pauseButton, {
+     onStart(){
+        console.log("Timer has started");
+     },
+     onTick() {
+        console.log("Timer just tick down");
+     },
+     onComplete() {
+        console.log("Timer is completed");
+     }
+    }
+);
